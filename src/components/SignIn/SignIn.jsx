ef25bb941785541b859a66f4da79a92af0520429
avanthik33 <avanthik33@gmail.com>
2024-03-26 17:20:27 +0530

    .\.git\

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./SignIn.css";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -8,9 +9,11 @@ const SignIn = () => {
     email: "",
     password: "",
   });
+
   const inputHandle = (event) => {
     setInput({ ...input, [event.target.name]: event.target.value });
   };
+
   const submitHandle = () => {
     axios.post("http://localhost:3001/admin/signin", input).then((response) => {
       if (response.data.message === "Student login success") {
@@ -27,46 +30,60 @@ const SignIn = () => {
         navigate("/staffDash");
       } else {
         alert(response.data.message);
-        setInput({
-          email: "",
-          password: "",
-        });
+        setInput({ email: "", password: "" });
       }
     });
   };
 
   return (
-    <div>
+    <div className="signin-container">
       <div className="container">
-        <div className="row g-3">
-          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <label htmlFor="" className="form-label">
-              Email
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="email"
-              value={input.email}
-              onChange={inputHandle}
+        <div className="row">
+          <h1 className="signin">SIGNIN</h1>
+        </div>
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <img
+              src="https://image.shutterstock.com/image-photo/graduation-cap-earth-globe-concept-260nw-2349898783.jpg"
+              alt=""
             />
           </div>
-          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <label htmlFor="" className="form-label">
-              Password
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="password"
-              value={input.password}
-              onChange={inputHandle}
-            />
-          </div>
-          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <button className="btn btn-success" onClick={submitHandle}>
-              SignIn
-            </button>
+          <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={input.email}
+                  name="email"
+                  onChange={inputHandle}
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={inputHandle}
+                  value={input.password}
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success" onClick={submitHandle}>
+                  SignIn
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
