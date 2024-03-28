5a2c8d1ec43fa30a93580467adf0f3ac3ddec0bb
avanthik33 <avanthik33@gmail.com>
2024-03-28 22:13:01 +0530

    .\.git\

--- src/components/AdminDashBord/ViewStaff.jsx
+++ src/components/AdminDashBord/ViewStaff.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import AdminNavBar from "../NavBar";
 
 const ViewStaff = () => {
   const [data, setData] = useState([]);
+  const length = data.length;
 
   const fetchStaffDetails = () => {
     try {
@@ -26,32 +28,39 @@ const ViewStaff = () => {
 
   return (
     <div>
+      <AdminNavBar profile="/adminProfile" user="/adminDash" />
       <div className="container">
         <div className="row">
-          <div className="col-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div className="row">
-              {data.map((value, index) => {
-                return (
-                  <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">{value.firstName}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">
-                          {value.email}
-                        </h6>
-                        <p class="card-text">
-                          Department: {value.department_id.department}
-                        </p>
-                        <Link to="/searchStaff" class="card-link">
-                          Card link
-                        </Link>
+          {length === 0 ? (
+            <>
+              <h1 style={{ fontFamily: "fantasy" }}>No data to show</h1>
+            </>
+          ) : (
+            <div className="col-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <div className="row">
+                {data.map((value, index) => {
+                  return (
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                      <div class="card">
+                        <div class="card-body">
+                          <h5 class="card-title">{value.firstName}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">
+                            {value.email}
+                          </h6>
+                          <p class="card-text">
+                            Department: {value.department_id.department}
+                          </p>
+                          <Link to="/searchStaff" class="card-link">
+                            Card link
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
